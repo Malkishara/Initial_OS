372d6844dfd6ba2305335e66187544130f1eefcc
Malkishara <65393614+Malkishara@users.noreply.github.com>
2021-08-06 20:46:21 +0530

    Add files via upload (#3)

--- kmain.c
+++ kmain.c
@@ -1,3 +1,14 @@
+#include "framebuffer.h"
+#include "serial.h"
+
+
+    
 void kmain(){
 
+    char msg[] = "Initial_OS";
+   
+    
+    serial_write(0x3F8, msg, 10);
+    fb_write(msg, 10);
+    
 }
